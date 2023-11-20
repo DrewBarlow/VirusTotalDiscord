e4fd9e3230c17d13d9d67d7ec53a2e5e5c15857b
Drew Barlow <drew.barlow@umbc.edu>
2023-11-20 11:22:45 -0500

    documentation and comments

--- src/virustotal.py
+++ src/virustotal.py
@@ -23,24 +23,37 @@ class VirusTotal(commands.Cog, name="virustotal"):
         return
 
     async def _scan_file(self: VirusTotal, file_url: str) -> vt.Object:
+        """
+        @param file_url (str): The url to the file to scan.
+        @return vt.Object: The scanned file.
+        """
+        # load the file into ram and get the md5 hash of it
         file_bytes: bytes = await self._fetch_file(file_url)
         file_hash: str = md5(file_bytes).hexdigest()
 
         file: Optional[vt.Object] = None
         try:
+            # check virustotal for the file if it's already present
             file = await self._vt.get_object_async(f"/files/{file_hash}")
         except vt.error.APIError:
+            # if the file is not in virustotal, upload and scan it
             file_stream: BytesIO = BytesIO(file_bytes)
             file = await self._vt.scan_file_async(file_stream, wait_for_completion=True)
 
         return file
 
-    async def _scan_url(self: VirusTotal, client: vt.Client, url: str) -> vt.Object:
+    async def _scan_url(self: VirusTotal, url: str) -> vt.Object:
+        """
+        @param url (str): The url to scan.
+        @return vt.Object: The scanned url.
+        """
         file: Optional[vt.Object] = None
         try:
+            # query virustotal for the url
             url_id: str = vt.url_id(url)
             file = await self._vt.get_object_async(f"/urls/{url_id}")
         except vt.error.APIError:
+            # scan the url manually if not present
             file = await self._vt.scan_url_async(url, wait_for_completion=True)
 
         return file
